956cf631b1cbb82d90b1ea74bedcf4cc9ae75e76
Rahyan Ashraf <ashraf.rahyan@gmail.com>
2026-06-17 15:57:47 +0600

    7th

---
+++ 4. User Input/user_input.py
@@ -0,0 +1,8 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
---
+++ 6. Arithmetic and Math/math.py
@@ -0,0 +1,17 @@
+x = 3.14
+y = -5
+z = 4
+
+# result = round(x)     gives 3
+# result = round(x, 2)  rounds to 2 decimal place
+# result = abs(y)       gives 5
+# result = pow(4,3)     means 4 to the power of 3
+# result = max(x, y, z) gives the max of x, y, z
+
+import math
+
+print(math.pi)
+print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x) gives 4
+# result = math.floor(x) gives 3
