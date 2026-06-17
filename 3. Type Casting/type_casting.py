eac3eb4a17f099f509aced3e0eb50677b375252c
Rahyan Ashraf <ashraf.rahyan@gmail.com>
2026-06-17 15:38:29 +0600

    nth commit

---
+++ 3. Type Casting/type_casting.py
@@ -0,0 +1,17 @@
+# Typecasting - the process of convering a variable  from one data type to another
+
+
+name = "Boss"
+age = 25
+gpa = 3.2
+is_Student = True
+
+print(type(gpa)) #age is float
+
+gpa = int(gpa)
+
+print(type(age)) #age is int
+
+age = str(age)
+
+print(type(age)) #age is str
