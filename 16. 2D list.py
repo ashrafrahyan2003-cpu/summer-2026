ec35006555cd68a64904d632bba202a3f9190b87
ashrafrahyan2003-cpu <ashraf.rahyan.2003@gmail.com>
2026-06-20 23:25:42 +0600

    2D lists

---
+++ 16. 2D list.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "orange", "pineapple"]
+vegetables = ["tomato", "eggplant"]
+
+grocery = [[fruits], [vegetables]]
+
+for i in grocery:
+    for j in i:
+        print(j)
